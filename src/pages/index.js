47ed2b4f396d01eb4b9c6b8af862e3aca7a9a45f
Amrutha Korumilli <Amrutha.Korumilli@c.hasbro.com>
2023-03-06 00:13:28 +0530

    events props added

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,12 @@ import Router, { useRouter } from 'next/router'
 export default function Main() {
     const router = new useRouter();
     ga.pageview(router?.asPath)
+    const props={action:"clicked",category:"search"}
   return (
     <>
     <div>Hello</div>
     <div>
-    <button onClick={() => ga.event("clicked","hello")}>Search</button>
+    <button onClick={() => ga.event({action:"clicked",category:"search",label:"search click",value:"value"})}>Search</button>
 </div>
 </>
   )
