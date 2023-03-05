9f6406c33acddf2360824b70a1b8a68e539a94f6
Amrutha Korumilli <Amrutha.Korumilli@c.hasbro.com>
2023-03-06 00:03:05 +0530

    router changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import * as ga from "../lib/ga"
-
+import Router, { useRouter } from 'next/router'
 export default function Main() {
-    ga.pageview("/home")
+    const router = new useRouter();
+    ga.pageview(router?.asPath)
   return (
     <>
     <div>Hello</div>
     <div>
-    <button onClick={() => ga.event("clicked")}>Search</button>
+    <button onClick={() => ga.event("clicked","hello")}>Search</button>
 </div>
 </>
   )
